fddb0c69bc8b7e89c4e1ebd1ad50d71c7f850079
Kirzahk <61593550+Kirzahk@users.noreply.github.com>
2020-03-01 20:46:47 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 
-namespace Ejemplo003___Enumeradores
+namespace Ejemplo004___TiposDatos
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
-            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double. MaxValue:N0}.");
-            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and canstore numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+            Console.WriteLine($"int emplea {sizeof(int)} bytes y puede almacenar números desde {int.MinValue:N0} a {int.MaxValue:N0}.");
+            Console.WriteLine($"double emplea {sizeof(double)} bytes y puede almacenar números desde {double.MinValue:N0} a {double. MaxValue:N0}.");
+            Console.WriteLine($"decimal emplea {sizeof(decimal)} bytes y puede almacenar números desde {decimal.MinValue:N0} a {decimal.MaxValue:N0}.");
         }
     }
 }
